acf0354723327fa0ad4bc77f4a2d9446a5f76330
wbusaka <wbusaka>
2019-04-02 01:01:52 +0000

    second commit

---
+++ game.js
@@ -0,0 +1,15 @@
+let player1 = prompt('Player One: Enter your name, you will be Blue')
+let player1Color = 'rgb(86, 151, 255)'
+
+let player1 = prompt('Player Two: Enter your name, you will be Red')
+let player2Color = 'rgb(237, 45, 73)'
+
+let game_on = true
+let table = $('table tr')
+
+
+const reportWin = (rowNum, colNum)=>{
+  console.log('You won starting at this row, col')
+  console.console.log(rowNum)
+  console.console.log(colNum);
+}
